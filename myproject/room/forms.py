78bebde29398157a09f9624d6a5f17cf3cddfa5e
Ukrit Dokkularb <6410535014@student.tu.ac.th>
2025-11-16 13:05:01 +0700

    refine create_room's UI

--- myproject/room/forms.py
+++ myproject/room/forms.py
@@ -2,9 +2,14 @@ from django import forms
 from .models import Room
 
 class RoomCreateForm(forms.ModelForm):
-	class Meta:
-		model = Room
-		fields = ['name','description']
+    class Meta:
+        model = Room
+        fields = ['name', 'description']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs.update({'class': 'form-control'})
 
 class JoinRoomByCodeForm(forms.Form):
 	code = forms.CharField(max_length=12)
